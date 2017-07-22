2e2f28d89ab74b84b7421f2170dc57775081f419
xiaobo <xiaobo.xwt@alibaba-inc.com>
2017-07-22 17:36:45 +0800

    day6

--- chart4/longstr.c
+++ chart4/longstr.c
@@ -3,7 +3,7 @@ int main(void){
     printf("Here is on way to print ");
     printf("a long string\n");
     printf("Here is a way to print a\
-    long string.\n");
+long string.\n");
     printf("here is a way to "
         "print a long string\n");
     return 0;
