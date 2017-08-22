3134231631c767ffb0e7fa57b5994a7435f56067
xiaobo <xiaobo.xwt@alibaba-inc.com>
2017-08-22 21:00:11 +0800

    day17

---
+++ chart10/day_mon1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define MONTHS 12
+int main(void){
+	const int day[MONTHS] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int index;
+
+	for(index = 0; index < MONTHS; index++){
+		printf("Month %2d is %2d days.\n",index+1,day[index]);
+	}
+
+	return 0;
+}
---
+++ chart10/designate.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define MONTHS 12
+int main(void){
+	int days[MONTHS] = {31, 28, [4] = 31, 30, 31};
+
+	for(int index = 0; index < MONTHS; index++){
+		printf("Month %2d is %10d days\n",index+1,days[index]);
+	}
+
+	return 0;
+}
