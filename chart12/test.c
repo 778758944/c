741c2043263910cb1f1c5f5872609998887c76e9
xiaobo <xiaobo.xwt@alibaba-inc.com>
2017-09-10 18:07:54 +0800

    modify

--- chart12/test.c
+++ chart12/test.c
@@ -3,9 +3,11 @@
 #include <time.h>
 
 int main(){
-	srand((unsigned int) time(0));
-	for(int i = 1; i < 10; i++){
-		printf("%d\n",rand());
-	}
+	// srand((unsigned int) time(0));
+	// for(int i = 1; i < 10; i++){
+	// 	printf("%d\n",rand());
+	// }
+	// return 0;
+	printf("sizeof %lu\n",sizeof(int));
 	return 0;
 }
