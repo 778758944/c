66592b57d1ba34e7af684464bd0133a6518391a0
xiaobo <xiaobo.xwt@alibaba-inc.com>
2017-09-12 22:03:10 +0800

    modify

--- chart11/repeat.c
+++ chart11/repeat.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int main(int argc,char *argv[]){
+int main(int argc,char *argv[]){//4个参数，第一次是命令行，其余都是字符串参数
 	int count;
 	printf("the command line has %d arguments:\n",argc -1);
-	for(count = 1; count < argc; count++){
+	for(count = 0; count < argc; count++){
 		puts(argv[count]);
 	}
 	return 0;
